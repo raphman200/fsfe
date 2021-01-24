da1a51faddc662af590da1697aad672d7e179fc3
raphman100 <raphman100@gmail.com>
2021-01-24 12:56:08 -0500

    Add a new route

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
         res.send('Hello The Raphael');
 });
 
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', '4life');
+	res.status(418).send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 
